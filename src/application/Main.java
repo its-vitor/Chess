7e52941e76ebba15e6cc4ea544ed901c7b835bc4
Calisto <vitor.redhat@outlook.com>
2024-04-14 21:51:00 -0300

    adicionado classe position

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,12 @@
 package application;
 
+import boardgame.Position;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+		Position p = new Position(1, 5);
+		System.out.println(p);
 	}
 
 }
