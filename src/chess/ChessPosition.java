f8b2613af017de8fc53d9c66d306be302d9066fb
Calisto <vitor.redhat@outlook.com>
2024-04-21 00:06:09 -0300

    adicionado ChessPosition

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -1,10 +1,13 @@
 package chess;
 
+import exceptions.ChessException;
+
 public class ChessPosition {
 	public char column;
 	public Integer row;
 	
 	public ChessPosition(char column, Integer row) {
+		if (column < 'a' || column > 'h' || row < 1 || row > 8) throw new ChessException("Erro de instanciação.");
 		this.column = column;
 		this.row = row;
 	}
