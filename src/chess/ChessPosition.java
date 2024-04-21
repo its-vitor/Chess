b816a25fe14ac30ac07c536eff731dad83b87fbe
Calisto <vitor.redhat@outlook.com>
2024-04-21 00:02:37 -0300

    adicionado ChessException

---
+++ src/chess/ChessPosition.java
@@ -0,0 +1,11 @@
+package chess;
+
+public class ChessPosition {
+	public char column;
+	public Integer row;
+	
+	public ChessPosition(char column, Integer row) {
+		this.column = column;
+		this.row = row;
+	}
+}
