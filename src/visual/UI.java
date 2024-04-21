fd0e5529f3ce8c9374e3f2b78d5e3c65d82c6c2b
Calisto <vitor.redhat@outlook.com>
2024-04-21 01:14:55 -0300

    adicionado readChessPosition

--- src/visual/UI.java
+++ src/visual/UI.java
@@ -1,6 +1,10 @@
 package visual;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class UI {
 	public static void printBoard(ChessPiece[][] pieces) {
@@ -12,6 +16,16 @@ public class UI {
 			System.out.println();
 		}
 		System.out.print("  A B C D E F J H");
+	}
+	
+	public static ChessPosition readChessPosition(Scanner sc) {
+		try {
+			String position = sc.nextLine();
+			return new ChessPosition(position.charAt(0), Integer.parseInt(position.substring(1)));
+		} catch (RuntimeException e) {
+			throw new InputMismatchException("Posição de leitura inválida.");
+			
+		}
 	}
 	
 	public static void printPiece(ChessPiece piece) {
