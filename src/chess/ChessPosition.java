0301c64ec290a6bec11795250c8bf7aca4bd2219
Calisto <vitor.redhat@outlook.com>
2024-04-21 00:19:39 -0300

    toPosition e fromPosition adicionados

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -1,5 +1,6 @@
 package chess;
 
+import boardgame.Position;
 import exceptions.ChessException;
 
 public class ChessPosition {
@@ -10,5 +11,21 @@ public class ChessPosition {
 		if (column < 'a' || column > 'h' || row < 1 || row > 8) throw new ChessException("Erro de instanciação.");
 		this.column = column;
 		this.row = row;
+	}
+
+	public char getColumn() {
+		return column;
+	}
+
+	public Integer getRow() {
+		return row;
+	}
+	
+	protected Position toPosition() {
+		return new Position(8 - row, column - 'a');
+	}
+	
+	protected static ChessPosition fromPosition(Position position) {
+		return new ChessPosition((char)('a' - position.getRow()), position.getColumn());
 	}
 }
